4250ef7d6ea93619ed2546e3a1515d83787a5256
shyamd <shyamd@lbl.gov>
2017-06-08 16:24:37 -0700

    another fix

--- atomate/qchem/workflows/core.py
+++ atomate/qchem/workflows/core.py
@@ -8,7 +8,7 @@ This module defines the base qchem workflows
 
 
 from atomate.utils.utils import get_logger, append_fw_wf
-from fireworks.core.firework import Firework
+from fireworks.core.firework import Workflow
 from atomate.qchem.fireworks.core import OptimizeFW
 __author__ = 'Shyam Dwaraknath'
 __email__ = 'shyamd@lbl.gov'
