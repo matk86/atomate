cc7fb2af2c827371eb85906275b2568594aa0d10
Kiran Mathew <kmathew@lbl.gov>
2017-10-04 15:43:28 -0700

    add elnes+xanes preset wf

--- atomate/feff/workflows/presets.py
+++ atomate/feff/workflows/presets.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-from atomate.feff.workflows.core import get_wf_xas
+from atomate.feff.workflows.core import get_wf_xas, get_wf_eels
 
 __author__ = 'Kiran Mathew'
 __email__ = 'kmathew@lbl.gov'
@@ -29,4 +29,28 @@ def wf_Xanes_K_edge(structure, c=None):
 
     wf = get_wf_xas(absorbing_atom, structure, edge="K", feff_cmd=feff_cmd, db_file=db_file,
                     metadata=metadata, user_tag_settings=user_tag_settings, use_primitive=False)
+    return wf
+
+
+def wf_Elnes_Xanes_K_edge(structure, c=None):
+
+    c = c or {}
+    feff_cmd = c.get("FEFF_CMD", "feff_mpi")
+    db_file = c.get("DB_FILE", None)
+    metadata = c.get("METADATA", {})
+    absorbing_atom = c.get("ABSORBING_ATOM")
+
+    user_tag_settings = {"RPATH": -1,
+                         "SCF": "7 0 30 0.2 3",
+                         "FMS": "9 0",
+                         "LDOS": "-30.0 30.0 0.1",
+                         "RECIPROCAL": "",
+                         "EDGE": "K",
+                         "COREHOLE": "RPA"}
+
+    metadata.update({"absorbing_atom_idx": str(absorbing_atom)})
+
+    wf = get_wf_eels(absorbing_atom, structure, edge="K", feff_cmd=feff_cmd, db_file=db_file,
+                     metadata=metadata, user_tag_settings=user_tag_settings, use_primitive=False,
+                     add_xas="XANES")
     return wf
